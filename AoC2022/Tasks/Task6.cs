43e34913083852d0f725fdf5d48b6aff8284fba8
Alexander Mills <alexander.mills@akra.de>
2022-12-06 14:14:16 +0100

    Task 6

---
+++ AoC2022/Tasks/Task6.cs
@@ -0,0 +1,52 @@
+﻿using AoCCommon;
+
+namespace AoC2022.Tasks;
+
+public class Task6 : ITask
+{
+    public override TaskResult RunPartOne()
+    {
+        var input = InitTaskString();
+        var buffer = new Queue<char>();
+
+        var i = 4;
+        foreach (var c in input)
+        {
+            buffer.Enqueue(c);
+            if (buffer.Count == 4)
+            {
+                if (buffer.Distinct().Count() == 4)
+                {
+                    return GetResult(i);
+                }
+                buffer.Dequeue();
+            }
+            i++;
+        }
+
+        return GetResult("");
+    }
+
+    public override TaskResult RunPartTwo()
+    {
+        var input = InitTaskString();
+        var buffer = new Queue<char>();
+
+        var i = 1;
+        foreach (var c in input)
+        {
+            buffer.Enqueue(c);
+            if (buffer.Count == 14)
+            {
+                if (buffer.Distinct().Count() == 14)
+                {
+                    return GetResult(i);
+                }
+                buffer.Dequeue();
+            }
+            i++;
+        }
+
+        return GetResult("");
+    }
+}
